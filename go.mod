08faab08bad01455db695c69bbd39921de776d3a
dairlair <dairlair@my.com>
2020-01-23 16:57:00 +0200

    Test update for training

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cheggaaa/pb/v3 v3.0.3
 	github.com/golang-migrate/migrate/v4 v4.7.0
 	github.com/jinzhu/gorm v1.9.11
+	github.com/lukechampine/randmap v0.0.0-20161125183226-9e3c222d0413
 	github.com/mattn/go-runewidth v0.0.7 // indirect
 	github.com/nats-io/gnatsd v1.4.1 // indirect
 	github.com/nats-io/go-nats v1.7.2 // indirect
@@ -19,4 +20,5 @@ require (
 	github.com/stretchr/testify v1.3.0
 	github.com/tidwall/gjson v1.4.0
 	github.com/tidwall/sjson v1.0.4
+	github.com/tjarratt/babble v0.0.0-20191209142150-eecdf8c2339d
 )
---
+++ pkg/domain/service/training/training_test.go
@@ -0,0 +1,84 @@
+package training
+
+import (
+	"fmt"
+	"github.com/dairlair/sentimentd/pkg/domain/entity"
+	"github.com/dairlair/sentimentd/pkg/domain/service/tokenizer"
+	"github.com/dairlair/sentimentd/pkg/infrastructure/model"
+	mocks "github.com/dairlair/sentimentd/pkg/mocks/domain/service/training"
+	"github.com/lukechampine/randmap/safe"
+	"github.com/stretchr/testify/mock"
+	"github.com/tjarratt/babble"
+	"testing"
+	"time"
+)
+
+func BenchmarkTrainingService_Train(b *testing.B) {
+	brainId := int64(1)
+	classes := generateClasses(10)
+	samples := generateSamples(classes, 10000000)
+
+	classService := mocks.ClassServiceInterface{}
+	class := &model.Class{
+		Model:   model.Model{ID:777},
+		BrainID: 1,
+		Label:   "qwerty",
+	}
+	classService.On("FindOrCreate", mock.AnythingOfType("int64"), mock.AnythingOfType("string")).Return(class, nil)
+
+	resultRepository := mocks.ResultsRepositoryInterface{}
+	resultRepository.On("SaveResult", brainId, mock.AnythingOfType("result.TrainingResult")).Return(nil)
+
+	tokenizerService := tokenizer.NewTokenizer()
+
+
+	tokenService := mocks.TokenServiceInterface{}
+	token := &model.Token{
+		Model:   model.Model{
+			ID:        1,
+			CreatedAt: time.Time{},
+			DeletedAt: nil,
+		},
+		BrainID: 1,
+		Text:    "text",
+	}
+	tokenService.On("FindOrCreate", mock.AnythingOfType("int64"), mock.AnythingOfType("string")).Return(token, nil)
+
+	service := NewTrainingService(&classService, &resultRepository, &tokenizerService, &tokenService)
+
+	_ = service.Train(brainId, samples, func() {})
+}
+
+func generateClasses(count int64) map[string]entity.ClassInterface {
+	classes := make(map[string]entity.ClassInterface, count)
+
+	for i := int64(0); i < count; i++ {
+		label := fmt.Sprintf("class_%d", i)
+		class := &model.Class{
+			Model:   model.Model{ID:count},
+			BrainID: 1,
+			Label:   label,
+		}
+		classes[label] = class
+	}
+
+	return classes
+}
+
+func generateSamples(classes map[string]entity.ClassInterface, count int64) []entity.Sample {
+	samples := make([]entity.Sample, count)
+	babbler := babble.NewBabbler()
+	babbler.Count = 25
+	babbler.Separator = " "
+
+	for i := int64(0); i < count; i++ {
+		class := randmap.Val(classes).(entity.ClassInterface)
+		sample := entity.Sample{
+			Sentence: babbler.Babble(),
+			Classes:  []string{class.GetLabel()},
+		}
+		samples[i] = sample
+	}
+
+	return samples
+}
