1a910ac55f4be48986ed9764a0f528ba93055887
dairlair <dairlair@my.com>
2020-02-03 12:15:24 +0200

    Update root_test.go

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,10 +1,12 @@
 package cmd
 
-import (
-	"testing"
-)
+// This package needs integration tests setup
 
-func BenchmarkRoot_Analyse(b *testing.B) {
-	rootCmd.SetArgs([]string{"analyse", "skynet", "Text should to be analysed"})
-	rootCmd.Execute()
-}
+// import (
+// 	"testing"
+// )
+
+// func BenchmarkRoot_Analyse(b *testing.B) {
+// 	rootCmd.SetArgs([]string{"analyse", "skynet", "Text should to be analysed"})
+// 	rootCmd.Execute()
+// }
