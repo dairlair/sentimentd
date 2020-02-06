8bf246eb182e8e0e34567c8eff039b1f5c1c8ff0
dairlair <51401608+dairlair@users.noreply.github.com>
2020-02-06 13:18:53 +0200

    Log Level Control (#25)
    Now you can choose the log level, log format.
    
    Docker image build and publishing added.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+########################################################################################################################
+# Final stage                                                                                                          #
+########################################################################################################################
+FROM alpine:latest
+RUN apk --no-cache add ca-certificates
+
+ADD ./build/sentimentd.linux-amd64 /bin/sentimentd
+RUN chmod +x /bin/sentimentd
+
+ADD ./schema /var/lib/sentimentd/schema
+
+# We create empty config which should be overriden with mount docker option.
+# Also you can change application config through environment variables.
+RUN mkdir /etc/sentimentd
+RUN touch /etc/sentimentd/sentimentd.yml
+
+CMD ["brain", "ls"]
+ENTRYPOINT ["/bin/sentimentd"]
--- cmd/root.go
+++ cmd/root.go
@@ -24,16 +24,9 @@ var (
 )
 
 func init() {
-	configureViper()
-	if err := viper.ReadInConfig(); err != nil {
-		log.Warn(err)
-	}
-	// Config is read, lest create application...
-	config := application.Config{
-		Database: struct{ URL string }{
-			URL: viper.GetString("database.url"),
-		},
-	}
+	config, _ := readConfig()
+	setLogFormat(config.Log.Format)
+	setLogLevel(config.Log.Level)
 	app = application.NewApp(config)
 	app.Init()
 
@@ -53,6 +46,24 @@ func Execute() error {
 	return rootCmd.Execute()
 }
 
+func readConfig() (application.Config, error) {
+	configureViper()
+	if err := viper.ReadInConfig(); err != nil {
+		log.Warn(err)
+	}
+	// Config is read, lest create application...
+	config := application.Config{
+		Log: struct{ Level, Format string }{
+			Level:  viper.GetString("log.level"),
+			Format: viper.GetString("log.format"),
+		},
+		Database: struct{ URL string }{
+			URL: viper.GetString("database.url"),
+		},
+	}
+	return config, nil
+}
+
 func configureViper() {
 	viper.AutomaticEnv()
 	viper.SetConfigName("sentimentd")
@@ -62,6 +73,30 @@ func configureViper() {
 	viper.AddConfigPath("./")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.SetDefault("database.url", "postgres://sentimentd:sentimentd@sentimentd:5432/sentimentd?sslmode=disable")
+	viper.SetDefault("log.level", "warn")
+	viper.SetDefault("log.format", "text")
+}
+
+func setLogLevel(logLevel string) {
+	level, err := log.ParseLevel(logLevel)
+	if err != nil {
+		log.Warningf("Unknown log level [%s]", logLevel)
+	} else {
+		log.SetLevel(level)
+		log.Infof("Log level: %s", logLevel)
+	}
+}
+
+func setLogFormat(logFormat string) {
+	switch logFormat {
+	case "json":
+		log.SetFormatter(&log.JSONFormatter{})
+		return
+	case "text":
+		log.SetFormatter(&log.TextFormatter{})
+		return
+	}
+	log.Warningf("Unknown log format [%s]", logFormat)
 }
 
 func getNATSStreaming(configPath string) (stan.Conn, string, string) {
